9fd20bebac6e18b44fd48cf388fe24fba90a5b0f
Andrew <andrewnguyen.nsw@gmail.com>
2024-06-25 19:28:35 +1000

    Update Resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 
 const page = () => {
-  redirect("https://drive.google.com/file/d/1VYYiurxXrMWn10sM4A07gWprmZDFXtNq/view");
+  redirect("https://drive.google.com/file/d/1bmItxtVVFCEpSP5slYCFCTIZu3DpD41f/view?usp=sharing");
 }
 
 export default page
